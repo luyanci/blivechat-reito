xfgryujk/blivechat

前端直连时认证包加上key

frontend/src/api/chat/ChatClientDirectWeb.js
@@ -16,6 +16,8 @@ export default class ChatClientDirectWeb extends ChatClientOfficialBase {
     this.hostServerList = [
       { host: "broadcastlv.chat.bilibili.com", port: 2243, wss_port: 443, ws_port: 2244 }
     ]
+    this.hostServerToken = null
+    this.buvid = ''
   }
 
   async initRoom() {
@@ -32,6 +34,8 @@ export default class ChatClientDirectWeb extends ChatClientOfficialBase {
     if (res.hostServerList.length !== 0) {
       this.hostServerList = res.hostServerList
     }
+    this.hostServerToken = res.hostServerToken
+    this.buvid = res.buvid
     return true
   }
 
@@ -56,8 +60,10 @@ export default class ChatClientDirectWeb extends ChatClientOfficialBase {
       protover: 3,
       platform: 'web',
       type: 2,
-      buvid: '',
-      // TODO B站开始检查key了，有空时加上
+      buvid: this.buvid,
+    }
+    if (this.hostServerToken !== null) {
+      authParams.key = this.hostServerToken
     }
     this.websocket.send(this.makePacket(authParams, base.OP_AUTH))
   }

luyanci/blivechat-reito

frontend/src/api/chat/ChatClientDirectWeb.js
@@ -16,6 +16,8 @@ export default class ChatClientDirectWeb extends ChatClientOfficialBase {
     this.hostServerList = [
       { host: "broadcastlv.chat.bilibili.com", port: 2243, wss_port: 443, ws_port: 2244 }
     ]
+    this.hostServerToken = null
+    this.buvid = ''
   }
 
   async initRoom() {
@@ -32,6 +34,8 @@ export default class ChatClientDirectWeb extends ChatClientOfficialBase {
     if (res.hostServerList.length !== 0) {
       this.hostServerList = res.hostServerList
     }
+    this.hostServerToken = res.hostServerToken
+    this.buvid = res.buvid
     return true
   }
 
@@ -54,6 +58,9 @@ export default class ChatClientDirectWeb extends ChatClientOfficialBase {
       room_id: this.roomId
       // TODO B站开始检查key了，有空时加上
     } })).data
+    if (this.hostServerToken !== null) {
+      authParam.key = this.hostServerToken
+    }
     this.websocket.send(this.makePacket(authParam, base.OP_AUTH))
   }
 